cc83fd5b10987b8d64465e19970acbd249ad8f81
Cael <2750417853@qq.com>
2022-10-23 00:43:39 +0800

    update login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,11 +7,29 @@ import { useStateContext } from "../context/ContextProvider";
 const Login = () => {
   const { setIsLogin } = useStateContext();
   const [isLoginFailed, setisLoginFailed] = useState(false);
-  const InputFields = document.getElementsByTagName("input");
+  const [form, setForm] = useState({ username: "", password: "" });
+  const onUpdateInput = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (form.username.length && form.username.length) {
+      loginApi(form.username, form.password, (data) => {
+        if (data.status) {
+          setIsLogin(true);
+          setisLoginFailed(false);
+        } else {
+          setIsLogin(false);
+          setisLoginFailed(true);
+        }
+      });
+    }
+  };
 
   return (
     <div className='flex flex-col items-center justify-center w-screen min-h-screen bg-gradient-to-br from-[#80ffdb] to-[#0077b6]'>
-      <div className='bg-white p-3 md:p-10 md:my-0 my-10 rounded-xl min-w-[90%] md:min-w-[600px] flex flex-col gap-4'>
+      <form
+        onSubmit={onSubmitForm}
+        className='bg-white p-3 md:p-10 md:my-0 my-10 rounded-xl min-w-[90%] md:min-w-[600px] flex flex-col gap-4'
+      >
         {isLoginFailed && (
           <div className='bg-red-500 text-white font-semibold mx-5 py-2 rounded-xl text-center'>
             <p>登录失败</p>
@@ -25,54 +43,36 @@ const Login = () => {
             用户名
           </label>
           <input
+            required
             type='text'
-            id='username'
             name='username'
             placeholder='用户名'
+            value={form.username}
+            onChange={onUpdateInput}
             className='p-4 bg-slate-100 rounded-xl outline-none'
           />
-          <div className='pl-4 text-red-500 err-msg'></div>
         </div>
         <div className='flex flex-col gap-2 px-4 w-full form-element'>
           <label htmlFor='password' className='pl-4'>
             密码
           </label>
           <input
+            required
             type='password'
-            id='password'
             name='password'
             placeholder='密码'
+            value={form.password}
+            onChange={onUpdateInput}
             className='p-4 bg-slate-100 rounded-xl outline-none'
           />
-          <div className='pl-4 text-red-500 err-msg'></div>
         </div>
         <div className='w-full text-center'>
-          <button
-            className='py-3 px-10 rounded-xl bg-[#FF0063] text-white font-semibold'
-            onClick={() => {
-              if (InputFields[0].value.length === 0)
-                InputFields[0].closest(".form-element").querySelector(".err-msg").innerText = "用户名不能为空哦";
-              else InputFields[0].closest(".form-element").querySelector(".err-msg").innerText = "";
-              if (InputFields[1].value.length === 0)
-                InputFields[1].closest(".form-element").querySelector(".err-msg").innerText = "密码不能为空哦";
-              else InputFields[1].closest(".form-element").querySelector(".err-msg").innerText = "";
-              if (InputFields[0].value.length && InputFields[1].value.length) {
-                loginApi(InputFields[0].value, InputFields[1].value, (data) => {
-                  if (data.status) {
-                    setIsLogin(true);
-                    setisLoginFailed(false);
-                  } else {
-                    setIsLogin(false);
-                    setisLoginFailed(true);
-                  }
-                });
-              }
-            }}
-          >
-            登录
-          </button>
+          <button className='py-3 px-10 rounded-xl bg-[#FF0063] text-white font-semibold'>登录</button>
         </div>
-      </div>
+        <a href='https://punch.mraddict.top' className='text-xs underline text-center text-[#7475BE]'>
+          返回提交页面
+        </a>
+      </form>
       <Footer />
     </div>
   );
